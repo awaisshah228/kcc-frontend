07740ae20e65d4c955b3718e2932bf6a69428825
awaisshah228 <m.awaisshah228@gmail.com>
2022-08-20 19:16:51 +0500

    updadted moded

--- src/components/stakingPage/VoteModel.tsx
+++ src/components/stakingPage/VoteModel.tsx
@@ -1,11 +1,8 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
 
-type Props = {
-  control: boolean;
-};
-export default function VoteModal(props: Props) {
-  let [isOpen, setIsOpen] = useState<boolean>(props.control);
+export default function VoteModal() {
+  let [isOpen, setIsOpen] = useState(false);
 
   function closeModal() {
     setIsOpen(false);
@@ -17,6 +14,16 @@ export default function VoteModal(props: Props) {
 
   return (
     <>
+      <div className="fixed inset-0 flex items-center justify-center">
+        <button
+          type="button"
+          onClick={openModal}
+          className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+        >
+          Open dialog
+        </button>
+      </div>
+
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
           <Transition.Child
